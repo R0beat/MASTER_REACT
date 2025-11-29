fe2f1c5dc48c87c858e6292501447adc24c993c5
Roberto Gonzalez Arriaga <roberto.gonzalez.a739@gmail.com>
2025-11-29 07:55:36 -0600

    Herencia v2

--- 04-JS_Basico/clases.js
+++ 04-JS_Basico/clases.js
@@ -1,4 +1,5 @@
 let datos = document.getElementById('main');
+
 class Coche {
     constructor(modelo, velocidad, anio) {
         this.modelo = modelo;
@@ -16,8 +17,26 @@ class Coche {
 
 }
 
-const coche1 = new Coche('BMW', 200, 2017)
-const coche2 = new Coche('AUDI', 100, 2018)
-const coche3 = new Coche('MERCEDES', 200, 2021)
-const coche4 = new Coche('RENAULT', 200, 2022)
+const coche1 = new Coche('BMW', 200, 2017);
+const coche2 = new Coche('AUDI', 100, 2018);
+const coche3 = new Coche('MERCEDES', 200, 2021);
+const coche4 = new Coche('RENAULT', 200, 2022);
+
+class Autobus extends Coche {
+    constructor(modelo, velocidad, antiguedad) {
+        super(modelo, velocidad, antiguedad);
+        this.altura = 5;
+    }
+
+    mostrarAltura() {
+        return `La altura del bus es : ${this.altura}`
+    }
+}
+
+const autobus1 = new Autobus('BMW', 200, 2017);
+const div = document.getElementById('main');
+
+div.innerHTML += `El altura del autobús es : ${autobus1.altura}`;
+
+console.log(autobus1);
 
--- 04-JS_Basico/herencia.js
+++ 04-JS_Basico/herencia.js
@@ -3,11 +3,17 @@ const COCHE = require('./clases');
 class Autobus extends COCHE{
     constructor(modelo, velocidad, antiguedad) {
         super(modelo, velocidad, antiguedad);
+        this.altura =5;
+    }
+
+    mostrarAltura(){
+        return `La altura del bus es : ${this.altura}`
     }
 }
 
 const autobus1 = new Autobus('BMW', 200, 2017);
 const div = document.getElementById('main');
 
-console.log(autobus1);
 div.innerHTML += `El autobús avanza: ${autobus1}`;
+
+console.log(autobus1);
